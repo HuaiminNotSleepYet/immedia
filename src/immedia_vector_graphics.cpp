83fabd10c86b35cbede96da3807eeb9985e9a6bd
ZhangHuaimin <HuaiminNotSleepYet@outlook.com>
2024-04-17 19:06:16 +0800

    VectorGraphics: Add element polyline.

--- src/immedia_vector_graphics.cpp
+++ src/immedia_vector_graphics.cpp
@@ -82,23 +82,37 @@ struct EllipseFilledArgs
     int   Segments;
 };
 
+struct PolylineArgs
+{
+    ImVec2* Points;
+    int     PointCount;
+    ImU32   Color;
+    float   Thinkness;
+    ImDrawFlags Flags;
+};
+
+#define POLYLINE_ARGS_SIZE(POLYLINE_ARGS) sizeof(PolylineArgs) + sizeof(ImVec2) * POLYLINE_ARGS->PointCount
+
 // The structure of VectorGraphicsElement
 // | 0 - 16           | 16 - 32 |
 // | args struct size | id      |
+// 
+// If args struct size is 0, the actual size needs to be able to be calculated from struct field.
 
-#define ELEMENT_INFO(ID, ARGS) ((0xFF & ID) << 16) | (0xFF & (uint16_t)sizeof(ARGS))
+#define ELEMENT_INFO(ID, ARGS_SIZE) ((0xFF & ID) << 16) | (uint16_t)ARGS_SIZE
 
 #define ELEMENT_ARGS_SIZE(ELEMENT) (int)ELEMENT & 0xFF
 
 enum class Element : int
 {
-    Line          = ELEMENT_INFO(1, LineArgs         ),
-    Rect          = ELEMENT_INFO(2, RectArgs         ),
-    RectFilled    = ELEMENT_INFO(3, RectFilledArgs   ),
-    Circle        = ELEMENT_INFO(4, CircleArgs       ),
-    CircleFilled  = ELEMENT_INFO(5, CircleFilledArgs ),
-    Ellipse       = ELEMENT_INFO(6, EllipseArgs      ),
-    Ellipsefilled = ELEMENT_INFO(6, EllipseFilledArgs)
+    Line          = ELEMENT_INFO(1, sizeof(LineArgs)         ),
+    Rect          = ELEMENT_INFO(2, sizeof(RectArgs)         ),
+    RectFilled    = ELEMENT_INFO(3, sizeof(RectFilledArgs)   ),
+    Circle        = ELEMENT_INFO(4, sizeof(CircleArgs)       ),
+    CircleFilled  = ELEMENT_INFO(5, sizeof(CircleFilledArgs) ),
+    Ellipse       = ELEMENT_INFO(6, sizeof(EllipseArgs)      ),
+    Ellipsefilled = ELEMENT_INFO(7, sizeof(EllipseFilledArgs)),
+    Polyline      = ELEMENT_INFO(8, 0                        ),
 };
 
 VectorGraphics::VectorGraphics(const ImVec2& size) :
@@ -139,6 +153,22 @@ void VectorGraphics::AddEllipse(const ImVec2& center, const ImVec2& radius, ImU3
 void VectorGraphics::AddEllipseFilled(const ImVec2& center, const ImVec2& radius, ImU32 col, float rot, int num_segments)
 { ADD_ELEMENT(Element::Ellipsefilled, EllipseFilledArgs) { center.x, center.y, radius.x, radius.y, rot, col, num_segments }; }
 
+void VectorGraphics::AddPolyline(const ImVec2* points, int num_points, ImU32 col, float thickness, ImDrawFlags flags)
+{
+    Elements.push_back(static_cast<int>(Element::Polyline));
+    int old_size = ElementArgs.size();
+    ElementArgs.resize(sizeof(PolylineArgs) + sizeof(ImVec2) * num_points + old_size);
+
+    PolylineArgs* args = reinterpret_cast<PolylineArgs*>(&ElementArgs[old_size]);
+    uint8_t* point_buffer = &ElementArgs[old_size] + sizeof(PolylineArgs);
+    memcpy(point_buffer, points, sizeof(ImVec2) * num_points);
+    args->Points = reinterpret_cast<ImVec2*>(point_buffer);
+    args->PointCount = num_points;
+    args->Color = col;
+    args->Thinkness = thickness;
+    args->Flags = flags;
+}
+
 void VectorGraphics::Show(const ImVec2& size) const
 {
     ImGuiWindow* window = ImGui::GetCurrentWindow();
@@ -163,6 +193,7 @@ void VectorGraphics::Show(const ImVec2& size) const
 
     while (element < element_end)
     {
+        int args_size = ELEMENT_ARGS_SIZE(*element);
         switch (*element)
         {
         case Element::Line:
@@ -235,12 +266,26 @@ void VectorGraphics::Show(const ImVec2& size) const
                                         args->Segments);
             break;
         }
+        case Element::Polyline:
+        {
+            VectorGraphics* p = const_cast<VectorGraphics*>(this);
+            const PolylineArgs* args = reinterpret_cast<const PolylineArgs*>(element_args);
+            p->PointBuffer.resize(args->PointCount);
+            for (size_t i = 0; i < args->PointCount; i++)
+                p->PointBuffer[i] = pos + offset + args->Points[i] * scale;
+            draw_list->AddPolyline(PointBuffer.Data, args->PointCount,
+                                   args->Color,
+                                   args->Flags,
+                                   args->Thinkness == 0 ? 1 : args->Thinkness * scale);
+            args_size = POLYLINE_ARGS_SIZE(args);
+            break;
+        }
         default:
             break;
         }
 
-        element_args += ELEMENT_ARGS_SIZE(*element);
         ++element;
+        element_args += args_size;
     }
 }
 
--- src/immedia_vector_graphics.h
+++ src/immedia_vector_graphics.h
@@ -26,12 +26,16 @@ public:
     void AddEllipse      (const ImVec2& center, const ImVec2& radius, ImU32 col, float thickness = 0.0f, float rot = 0.0f, int num_segments = 0);
     void AddEllipseFilled(const ImVec2& center, const ImVec2& radius, ImU32 col,                         float rot = 0.0f, int num_segments = 0);
 
+    void AddPolyline(const ImVec2* points, int num_points, ImU32 col, float thickness = 0.0f, ImDrawFlags flags = 0);
+
     void Show(const ImVec2& size) const;
 
 private:
     ImVec2            Size;
     ImVector<int>     Elements;
     ImVector<uint8_t> ElementArgs;
+
+    ImVector<ImVec2>  PointBuffer;
 };
 
 }
